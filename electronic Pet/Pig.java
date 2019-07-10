ad8685bbd658c509cfff6a98c96c2f44aab6c941
logic <42250221+logicccc@users.noreply.github.com>
2019-07-10 10:46:59 +0800

    Create Pig.java

---
+++ electronic Pet/Pig.java
@@ -0,0 +1,11 @@
+public class Pig extends Pet {
+	public Pig (String name) {
+		super(name);
+	}
+	public Pig(String name, int full, int happy) {
+		super(name,full,happy);
+	}
+	public String cry() {
+		return "moumou~";
+	}
+}
